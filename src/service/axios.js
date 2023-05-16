29fd4b326ef27be3ac3d37e0aa5c6a9241478f5e
FarruxDEV <farruxbeknozimov@gmail.com>
2023-05-16 22:55:27 +0500

    fix baseURL

--- src/service/axios.js
+++ src/service/axios.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://localhost:7000/api/' //'https://megacrm-api-farruxbeknozimov.vercel.app/api/'
+axios.defaults.baseURL = 'https://megacrm-api-farruxbeknozimov.vercel.app/api/'
+// 'http://localhost:7000/api/'
 export default axios
